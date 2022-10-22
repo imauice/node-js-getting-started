a96c06ca147ef86850f0d5d2a044911cf7e6d787
imauice <102501547+imauice@users.noreply.github.com>
2022-10-22 16:07:03 +0700

    update routes

--- index.js
+++ index.js
@@ -35,6 +35,43 @@ app.get('/top-headlines', (req, res) => {
 
 })
 
+app.get('/entertainment',(req,res) =>{
+
+  var config = {
+    method: 'get',
+    url: 'https://newsapi.org/v2/top-headlines?country=th&apiKey=d1ce6f9ee18d4edcae3c9a15ed977a17&category=entertainment',
+    headers: { }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+
+})
+
+app.get('/technology',(req,res)=>{
+  var config = {
+    method: 'get',
+    url: 'https://newsapi.org/v2/top-headlines?country=th&apiKey=d1ce6f9ee18d4edcae3c9a15ed977a17&category=technology',
+    headers: { }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
