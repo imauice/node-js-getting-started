004c10dcf9d77f972b37c19791ccb0f9145cdda5
imauice <102501547+imauice@users.noreply.github.com>
2022-10-22 11:55:33 +0700

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var axios = require('axios');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+
+app.get('/top-headlines', (req, res) => {
+
+  var config = {
+    method: 'get',
+    url: 'https://newsapi.org/v2/top-headlines?country=th&apiKey=d1ce6f9ee18d4edcae3c9a15ed977a17',
+    headers: { }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log('res ok');
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+
+})
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
