45e11a9e21b6df7d78a54de9941b00f8f36072c6
imauice <102501547+imauice@users.noreply.github.com>
2022-10-22 12:01:21 +0700

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var axios = require('axios');
+
 const app = express();
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
